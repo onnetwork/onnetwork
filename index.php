05543740351b9aa9e5d992490c962c5bd5374613
onnetwork <57286026+onnetwork@users.noreply.github.com>
2019-11-02 22:18:25 +0200

    intial commit

---
+++ index.php
@@ -0,0 +1,325 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>flex</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+         <nav class="nav navbar navbar-expand-xl navbar-light bg-light">        
+          <li class="logo"><a href="index.php"><img src="" alt="hold" class="white"><img src="" alt="hold" class="black"></a></li>
+          <button class="navbar-toggler nav-icon" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">    
+             <span class="navbar-toggler-icon"></span><span></span><span></span>
+          </button>
+         <div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-items"><a href="index.php" class="nav-link" id="">home</a></li>
+          <li class="nav-items"><a href="#our-services" class="nav-link">our services</a></li>
+          <li class="nav-items"><a href="#tech-news" class="nav-link">tech-news</a></li>
+          <li class="nav-items"><a href="#contact-us" class="nav-link">contact us</a></li>
+        </ul>
+      </div>                         
+      </nav>
+
+<div class="slider">
+<div id="carouselFade" class="carousel slide carousel-fade" data-ride="carousel"  data-interval="5000" data-pause="false">
+<ol class="carousel-indicators">
+    <li data-target="#carouselFade" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselFade" data-slide-to="1"></li>
+    <li data-target="#carouselFade" data-slide-to="2"></li>
+ </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100 imgs" src="images/slide.jpg" alt="First slide">
+      <div class="carousel-caption">
+        <h1>Climb mountains</h1>
+        <h2>Let us help you</h2>
+        <p>Archieve higher goals through our main plan</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 imgs" src="images/slid.jpg" alt="Second slide">
+      <div class="carousel-caption">
+        <h1>Get Digital DNA</h1>
+        <h2>Let us help you</h2>
+        <p>Define your blood stream online let people know who your are today!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 imgs" src="images/slider.jpg" alt="Third slide">
+      <div class="carousel-caption">
+        <h1>Imbress Tech</h1>
+        <h2>The world is evolving by the minute</h2>
+        <p justify-text-center>Have reliable online secucurity agents like us, is of essence,<br> we are here you, just click the button below and be covered.</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+  <div class="jumbotron jheader" id="our-services">
+    <h3 class="text-center shadow p-3 mb-5 bg-grey rounded">Welcome!</h3>
+  </div>
+        <div class="container ourservices">
+  <div class="row">
+     <div class="col-md-4 col-sm-4 text-center">
+      <div class="col-md-12 col-sm-12 shadow p-3 mb-5 bg-white rounded call-to-action">
+            <i class="fa fa-shield fa-5x"></i>
+           <img src="images/ImgResponsive_Placeholder.png" class="rounded-circle img-responsive center-block" alt="Placeholder image">
+           <h3 class="zero">Our speciality</h3>
+           <p class="descr">Write something</p>
+       </div>
+    </div>
+
+      <div class="col-md-4 col-sm-4 text-center">
+      <div class="col-md-12 col-sm-12 shadow p-3 mb-5 bg-white rounded call-to-action">
+          <img src="images/ImgResponsive_Placeholder.png" class="rounded-circle img-responsive center-block" alt="Placeholder image">
+          <h3 class="one">Our speciality</h3>
+          <p class="descr">Write something</p>
+        </div>
+    </div>
+
+      <div class="col-md-4 col-sm-4 text-center">
+      <div class="col-md-12 col-sm-12 shadow p-3 mb-5 bg-white rounded call-to-action">
+           <img src="images/ImgResponsive_Placeholder.png" class="rounded-circle img-responsive center-block" alt="Placeholder image">
+           <h3 class="two">Our speciality</h3>
+           <p class="descr"  id="tech-news">Write something</p>
+        </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+<!-- start of cards -->
+<!-- <main id="contents" role="main"> -->
+  <!-- <section id="about-us"> -->
+    <div class="container news cards">
+      <!-- <div class="row"> -->
+        <div class="text-center">
+          <h3>News Feed</h3>
+          <p class="lead pb-5">
+            Get the latest tech-news.
+          </p>  
+        </div>  
+          <div id="carouselServices" class="carousel slide slido">
+
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                 <div class="row">
+
+                <div class="col-md-4 col-sm-6">
+                  <div class="text-dark pb-3">
+                     <h4 class="text-dark text-center">WWDWKD</h4>                     
+           <!-- <div class="col-md-4 col-sm-6"> -->
+                     <div class="card bg-dark text-white">
+                   <img src="..." class="card-img cards-img" alt="...">
+                   <div class="card-img-overlay">
+                     <h5 class="card-title">Card title</h5>
+                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text">Last updated 3 mins ago</p>
+                   </div>
+                 </div>
+          <!-- </div> -->
+              </div>
+           </div>
+
+           <div class="col-md-4 col-sm-6">
+                  <div class="text-dark pb-3">
+                     <h4 class="text-dark text-center">WWDWKD</h4>                     
+           <!-- <div class="col-md-4 col-sm-6"> -->
+                     <div class="card bg-dark text-white">
+                   <img src="..." class="card-img cards-img" alt="...">
+                   <div class="card-img-overlay">
+                     <h5 class="card-title">Card title</h5>
+                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text">Last updated 3 mins ago</p>
+                   </div>
+                 </div>
+          <!-- </div> -->
+              </div>
+           </div>
+
+            <div class="col-md-4 col-sm-6">
+                  <div class="text-dark pb-3">
+                     <h4 class="text-dark text-center">WWDWKD</h4>                     
+           <!-- <div class="col-md px-md-5"> -->
+                     <div class="card bg-dark text-white">
+                   <img src="..." class="card-img cards-img" alt="...">
+                   <div class="card-img-overlay">
+                     <h5 class="card-title">Card title</h5>
+                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text">Last updated 3 mins ago</p>
+                   </div>
+                 </div>
+          <!-- </div> -->
+              </div>
+           </div>
+
+                </div>
+              </div> 
+
+          <div class="carousel-item">
+            <div class="row">
+  <!-- beggining of cards -->
+           <div class="col-md-4 col-sm-6">
+                  <div class="text-dark pb-3">
+                     <h4 class="text-dark text-center">WWDWKD</h4>                     
+           <!-- <div class="col-md px-md-5"> -->
+                     <div class="card bg-dark text-white">
+                   <img src="..." class="card-img cards-img" alt="...">
+                   <div class="card-img-overlay">
+                     <h5 class="card-title">Card title</h5>
+                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text">Last updated 3 mins ago</p>
+                   </div>
+                 </div>
+          <!-- </div> -->
+              </div>
+           </div>
+
+
+           <div class="col-md-4 col-sm-6">
+                  <div class="text-dark pb-3">
+                     <h4 class="text-dark text-center">WWDWKD</h4>                     
+           <!-- <div class="col-md px-md-5"> -->
+                     <div class="card bg-dark text-white">
+                   <img src="..." class="card-img cards-img" alt="...">
+                   <div class="card-img-overlay">
+                     <h5 class="card-title">Card title</h5>
+                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text">Last updated 3 mins ago</p>
+                   </div>
+                 </div>
+          <!-- </div> -->
+              </div>
+           </div>
+           
+           <div class="col-md-4 col-sm-6">
+                  <div class="text-dark pb-3">
+                     <h4 class="text-dark text-center">WWDWKD</h4>                     
+           <!-- <div class="col-md px-md-5"> -->
+                     <div class="card bg-dark text-white">
+                   <img src="..." class="card-img cards-img" alt="...">
+                   <div class="card-img-overlay">
+                     <h5 class="card-title">Card title</h5>
+                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text">Last updated 3 mins ago</p>
+                   </div>
+                 </div>
+          <!-- </div> -->
+              </div>
+           </div>
+<!-- end of cards -->
+          </div>
+         </div>
+       </div>
+             
+             <ol class="carousel-indicators">
+              <li data-target="#carouselServices" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselServices" data-slide-to="1"></li>
+              <li data-target="#carouselServices" data-slide-to="2"></li>
+            </ol>
+
+             <a class="carousel-control-prev" href="#carouselServices" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselServices" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a> 
+
+      </div>        
+        <!-- </div> -->
+      <!-- </div> -->
+    </div><!-- 
+  </section>  
+</main> -->
+
+
+<div class="container well col-md" id="contact-us">
+  <footer>
+
+  <h3 class="contact text-center" href="#contact-us">Contact Us</h3>
+    <div class="row">
+       <div class="col-md-6">
+   <!-- <div class="col-md-9 col-md-offset-3 col-sm-12"> -->
+  <form action="#" method="post" role="form">
+    <div class="form-group">
+      <label for="fullname">Name:</label>
+      <input type="text" class="form-control" id="fullname" placeholder="Enter your name">
+    </div>
+    
+    <div class="form-group">
+      <label for="email">Email Address:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter your email">
+    </div>
+    
+    <div class="form-group">
+      <label for="fullname">Telephone:</label>
+      <input type="tel" class="form-control" id="fullname" placeholder="">
+    </div>
+    
+    <div class="form-group">
+      <label for="message">Message:</label>
+      <textarea class="form-control" id="message" rows="3"></textarea> 
+    </div>
+    <button type="submit" class="btn btn-primary btn-sm">Submit</button>
+  </form>
+</div>
+
+
+
+      <div class="col-md-6">
+        <div class="spacing">
+    
+          
+        </div>
+        <div class="row">
+          <div class="col-sm-4 col-md-4 col-lg-4 col-xs-6">
+            <div>
+              <ul class="list-unstyled">
+                <li> <br></li>
+                <li> <a>Link anchor</a> </li>
+                <li> <a>Link anchor</a> </li>
+                <li> <a>Link anchor</a> </li>
+                <li> <a>Link anchor</a> </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-5"> 
+        <address>
+        <strong>MyStoreFront, Inc.</strong><br>
+        Indian Treasure Link<br>
+        Quitman, WA, 99110-0219<br>
+        <abbr title="Phone">P:</abbr> (123) 456-7890
+      </address>
+        <address>
+        <strong>Full Name</strong><br>
+        <a href="mailto:#">first.last@example.com</a>
+        </address>
+        </div>
+        <ul class="social-icons list-inline col-md-9 " style="justify-content: center;">
+          <li><a href=""><i class="fa fa-facebook"></i></a></li>
+          <li><a href=""><i class="fa fa-facebook"></i></a></li>
+          <li><a href=""><i class="fa fa-facebook"></i></a></li>
+          <li><a href=""><i class="fa fa-facebook"></i></a></li>
+        </ul>
+        </div>
+      </div>
+     
+    </div>
+    <div class="copy col-md-4 text-center">
+      &copy;Onnet media
+    </div>
+  </footer>
+</div>
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	
+	$(window).scroll(function(){
+		var sc = $(window).scrollTop();
+		  if(sc > 100){
+		  	$(".nav").addClass("sticky");
+		  }
+		  else{
+		  	$(".nav").removeClass("sticky");
+		  }
+	});
+});
